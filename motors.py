e1092552579f054f718ca15bab6b3058690e1bd6
AriadnaHC <65170456+AriadnaHCTec@users.noreply.github.com>
2022-10-03 19:59:07 -0500

    pin corrections

--- motors.py
+++ motors.py
@@ -2,20 +2,25 @@ import random
 from machine import Pin, PWM
 import utime
 
+def _map(x, in_min, in_max, out_min, out_max):
+    return int((x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min)
+
 def main():
     while True:        
-        #entre 0 y 65535
-        left_wheel.duty_u16(1600)
-        right_wheel.duty_u16(1600)
-        utime.sleep(.1)
+        per_izq = _map(0,-100,100,2000000,1000000)
+        #1500*1000#1600 atras 1400 adelante, 1500 stop
+        per_der = _map(0,-100,100,2000000,1000000)
+        left_wheel.duty_ns(per_der)
+        right_wheel.duty_ns(per_izq)
+        #utime.sleep(.1)
 
 # Activation
 # Declaration of 2 pwm (21,22)
-frequency = 1000  #10Khz
-left_wheel = PWM(Pin(16))
+frequency = 300  #10Khz
+left_wheel = PWM(Pin(17))
 left_wheel.freq(frequency)
-right_wheel = PWM(Pin(17))
+right_wheel = PWM(Pin(16))
 right_wheel.freq(frequency)
-#led.duty_u16(1)
+
 
 main()
--- test_sensor.py
+++ test_sensor.py
@@ -4,14 +4,14 @@ import utime
 
 def main():
     while True:
-        read_ground()
+        read_button()
         utime.sleep(.1)
 
 def read_ground():
-    global ground_front, ground_back
+    global ground_front, ground_back, ground
     for i in range(2):
-        ground_front = ground[i].value()
-        ground_back = ground[i+2].value()
+        ground_front[i] = ground[i].value()
+        ground_back[i] = ground[i+2].value()
     print("ground_front: "+str(ground_front)+"      ground_back: "+str(ground_back))
 
 def read_laser():    
@@ -19,10 +19,10 @@ def read_laser():
     #[31,32,34,2,5]
     lateral[0] = laser[3].value()
     lateral[1] = laser[4].value()
-    lateral_front[0] = laser[2].value()
-    lateral_front[1] = laser[1].value()
+    lateral_front[0] = laser[1].value()
+    lateral_front[1] = laser[2].value()
     front = laser[0].value()
-    print("lateral: "+str(laterakl)+"      lateral_front: "+str(lateral_front)+"       front: "+str(front))
+    print("lateral: "+str(lateral)+"      lateral_front: "+str(lateral_front)+"       front: "+str(front))
 
 def read_buttons():
     global buttons_value
@@ -31,34 +31,23 @@ def read_buttons():
     print("buttons: "+str(buttons_value))
 
 def read_button():
-    print(on_button.value())
-    
-poss_velocities = {"low":0,"medium":15,"high":30}
-left_velocity = poss_velocities["medium"]
-right_velocity = poss_velocities["medium"]
+    print(str(on_button.value()))
 
 # Declaration of 4 ground sensors (10,12,14,16)
-ground = [Pin(x, Pin.IN) for x in [7,9,10,12]]
+"""ground = [Pin(x, Pin.IN) for x in [7,9,10,12]]
 ground_front = [0]*2
-ground_back = [0]*2
-
-# Activation
-# Declaration of 2 pwm (21,22)
-"""frequency = 1000  #10Khz
-left_wheel = PWM(Pin(16))
-left_wheel.freq(frequency)
-right_wheel = PWM(Pin(17))
-right_wheel.freq(frequency)"""
-#led.duty_u16(1)
+ground_back = [0]*2"""
 
 # Declaration of 5 laser sensors  (31,32,34,2,5)
-laser = [Pin(x, Pin.IN) for x in [26,27,28,1,3]]
-lateral = [0]*2, lateral_front = [0]*2, front = 0
+"""laser = [Pin(x, Pin.IN) for x in [26,27,28,1,3]]
+lateral = [0]*2
+lateral_front = [0]*2
+front = 0"""
 
 # Declaration of 4 buttons (24-27)
-buttons = [Pin(x, Pin.IN, Pin.PULL_UP) for x in range(18,22)]
-buttons_value = [0]*len(buttons)
-on_button = Pin(12, Pin.IN, Pin)
+"""buttons = [Pin(x, Pin.IN, Pin.PULL_UP) for x in range(18,22)]
+buttons_value = [0]*len(buttons)"""
+on_button = Pin(4, Pin.IN)
 
 #configuration()
 main()
