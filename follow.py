698ef7fe778b29dc3310b2cd8ffd55693743f612
AriadnaHC <65170456+AriadnaHCTec@users.noreply.github.com>
2022-10-05 19:24:07 -0500

    Follow and search

--- follow.py
+++ follow.py
@@ -10,34 +10,89 @@ def main():
         if not on_button.value():
             print("nel")
             put_velocity(0, 0)
-            utime.sleep(.001)    
+            utime.sleep(.001)
         else:        
-            print("osiosi")
             read_values()
             
-            if 1 in lateral_front:
+
+            if 0 in ground_front:
+                search()
+                put_state()
+            elif 0 in ground_back:
+                #print(ground_back)
+                if ground_back[0] == 0 and ground_back[1] == 0:
+                    #print("rapidismo al frente")
+                    vel = 100
+                    put_velocity(vel,vel)
+                    counter_b = 30
+                elif ground_back[0] == 0:
+                    #print("Gira derecha")
+                    put_velocity(80,60)
+                    counter_b = 15
+                elif ground_back[1] == 0:
+                    #print("Gira Izquierda")
+                    put_velocity(60,80)
+                    counter_b = 15
+            elif 1 == front:
+                #print("frente")
+                state == "straight"
+                put_state()
+            elif 1 in lateral_front:
                 if lateral_front[0]:
-                    print("Gira Izquierda")
-                    put_velocity(20,40)
+                    #print("Gira Izquierda")
+                    put_velocity(20,60)
                     last = "left"
                 else:
-                    print("Gira derecha")
-                    put_velocity(40,20)
+                    #print("Gira derecha")
+                    put_velocity(60,20)
                     last = "right"
-            elif 1 == front:
-                put_velocity(0,0)
+            elif 1 in lateral:
+                vel = 60
+                if lateral[0]:
+                    #print("Gira Izquierda recio")                    
+                    put_velocity(-vel,vel)
+                    last = "left_recio"
+                else:
+                    #print("Gira derecha recio")
+                    put_velocity(vel,-vel)
+                    last = "right_recio"
             else:
                 if last == " ":
-                    put_velocity(0,0)
+                    search()
+                    put_state()
                 elif last == "right":
                     put_velocity(40,20)
                 elif last == "left":
                     put_velocity(20,40)
+                elif last == "left_recio":
+                    put_velocity(-60,60)
+                elif last == "right_recio":
+                    put_velocity(60,-60)
                 counter_b-=1
-                
-            
-            print(state)
+                            
             utime.sleep(.001)
+
+def search():
+    """Random Search"""
+    global state, counter, ground_front, ground_back , last
+    if 0 in ground_front:
+        counter = 80
+        state = "back"
+        if ground_front[0] == 0:
+            last = "turn_left"
+        else:
+            last = "turn_right"
+    elif state == 'turn_left' or state == "turn_right":
+        if counter > 0:
+            counter -= 1     
+        else:
+            state = 'straight'
+    elif state == "back":
+        if counter > 0:
+            counter -= 1                  
+        else:
+            counter  = 110#Turn
+            state = last
 
 def _map(x, in_min, in_max, out_min, out_max):
     return int((x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min)
@@ -64,7 +119,7 @@ def put_state():
         put_velocity(vel,vel)
 
 def read_values():
-    #read_ground()
+    read_ground()
     read_laser()
 
 
@@ -74,17 +129,17 @@ def read_ground():
     ground_front[0] = ground[3].value()
     ground_back[0] = ground[1].value()
     ground_back[1] = ground[0].value()
-    print("ground_front: "+str(ground_front)+"      ground_back: "+str(ground_back))
+    #print("ground_front: "+str(ground_front)+"      ground_back: "+str(ground_back))
 
 def read_laser():    
     global laser_value, lateral, lateral_front, front
     #[31,32,34,2,5]
-    lateral[0] = laser[3].value()
-    lateral[1] = laser[4].value()
+    lateral[0] = laser[4].value()
+    lateral[1] = laser[3].value()
     lateral_front[0] = laser[1].value()
     lateral_front[1] = laser[2].value()
     front = laser[0].value()
-    print("lateral: "+str(lateral)+"      lateral_front: "+str(lateral_front)+"       front: "+str(front))
+    #print("lateral: "+str(lateral)+"      lateral_front: "+str(lateral_front)+"       front: "+str(front))
 
 # Declaration of 4 ground sensors (10,12,14,16)
 ground = [Pin(x, Pin.IN) for x in [7,9,10,12]]#1 black 0 blanco
