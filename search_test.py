cefe1408768af0fc0c11c5ff6b226d3fad6f5457
AriadnaHC <65170456+AriadnaHCTec@users.noreply.github.com>
2022-10-08 09:19:09 -0500

    Friday_last

--- search_test.py
+++ search_test.py
@@ -3,53 +3,76 @@ from machine import Pin, PWM
 import utime
 
 def main(): 
-    global state, ground_back, ground_front, counter, counter_b
+    global state, ground_back, ground_front, counter, counter_b, last
     global laser_value, lateral, lateral_front, front, last_laser
+    vel_ini = 80
+    started = False
     while True:
-        if not on_button.value():
+        if not on_button.value() and buttons_value[0] == 0:
             print("nel")
             put_velocity(0, 0)
+            read_values()
             utime.sleep(.01)    
         else:        
             read_values()
-            
-            if 0 in ground_front:
-                search()
-                put_state()
-            elif 0 in ground_back:
-                #print(ground_back)
+                       
+            if 0 in ground_back:
+                started = True
+                last_laser = " "
                 if ground_back[0] == 0 and ground_back[1] == 0:
-                    print("rapidismo al frente")
                     vel = 75
                     put_velocity(vel,vel)
                     counter_b = 30
                 elif ground_back[0] == 0:
-                    print("Gira derecha")
-                    put_velocity(75,20)
+                    put_velocity(75,50)
                     counter_b = 20
                 elif ground_back[1] == 0:
-                    print("Gira Izquierda")
-                    put_velocity(20,75)
+                    put_velocity(50,75)
                     counter_b = 20
+            elif 0 in ground_front:
+                started = True
+                last_laser = " "
+                search()
+                put_state()
+                print(state)
             elif state == "back":
+                started = True
                 if counter > 0:
-                    counter-=1
+                    counter -= 1                  
                 else:
-                    state = "straight"
+                    if last == "both":
+                        counter  = 25#Turn
+                    else:                
+                        counter  = 20#Turn
+                    state = last
+                put_state()            
             elif 1 == front:
-                #print("frente")
+                started = True
                 state = "straight"
                 put_state()
                 last_laser = " "
             elif 1 in lateral_front:
+                started = True
                 if lateral_front[0]:
-                    #print("Gira Izquierda")
                     put_velocity(20,75)
                     last_laser = "left"
                 else:
-                    #print("Gira derecha")
                     put_velocity(75,20)
                     last_laser = "right"
+            elif 1 in lateral:
+                started = True
+                state = 'straight'
+                vel = 80
+                if lateral[0]:
+                    #print("Gira Izquierda recio")                    
+                    put_velocity(-vel,vel)
+                    last = "left_recio"
+                else:
+                    #print("Gira derecha recio")
+                    put_velocity(vel,-vel)
+                    last = "right_recio"
+            elif not started:
+                put_velocity(-vel_ini,vel_ini)
             elif counter_b == 0:
                 if last_laser == " ":
                     search()
@@ -60,11 +83,17 @@ def main():
                 elif last_laser == "left":
                     state = 'straight'
                     put_velocity(20,75)
+                elif last == "left_recio":
+                    state = 'straight'
+                    vel = 75
+                    put_velocity(-vel,vel)
+                elif last == "right_recio":
+                    vel = 75
+                    state = 'straight'
+                    put_velocity(vel,-vel)
             else:
-                print("Atras")
                 counter_b-=1
                 
-            #print(state)
             utime.sleep(.01)
         
 def search():
@@ -74,6 +103,8 @@ def search():
         if 0 in ground_front:
             counter = 20
             state = "back"
+            if ground_front[0] == 0 and ground_front[1] == 0:
+                last  = "both"
             if ground_front[0] == 0:
                 last = "turn_right"
             else:
@@ -87,7 +118,10 @@ def search():
         if counter > 0:
             counter -= 1                  
         else:
-            counter  = 100#Turn
+            if last == "both":
+                counter  = 25#Turn
+            else:                
+                counter  = 20#Turn
             state = last
 
 def _map(x, in_min, in_max, out_min, out_max):
@@ -117,6 +151,7 @@ def put_state():
 def read_values():
     read_ground()
     read_laser()
+    read_buttons()
 
 def read_ground():
     global ground_front, ground_back, ground
@@ -134,14 +169,24 @@ def read_laser():
     lateral_front[0] = laser[1].value()
     lateral_front[1] = laser[2].value()
     front = laser[0].value()
-    print("lateral: "+str(lateral)+"      lateral_front: "+str(lateral_front)+"       front: "+str(front))
+    #print("lateral: "+str(lateral)+"      lateral_front: "+str(lateral_front)+"       front: "+str(front))
+
+def read_buttons():
+    global buttons_value
+    for i in range(len(buttons)):
+        buttons_value[i] = buttons[i].value()
+    print("buttons: "+str(buttons_value))
 
 # Declaration of 4 ground sensors (10,12,14,16)
 ground = [Pin(x, Pin.IN) for x in [7,9,10,12]]#1 black 0 blanco
 ground_front = [0]*2
 ground_back = [0]*2
 last = ""
-on_button = Pin(4, Pin.IN)
+
+# Declaration of 4 buttons (24-27)
+buttons = [Pin(x, Pin.IN, Pin.PULL_UP) for x in range(18,22)]
+buttons_value = [0]*len(buttons) #0 = on
+on_button = Pin(4, Pin.IN) # arriba apagado, abajo prendido 
 
 # Declaration of 5 laser sensors  (31,32,34,2,5)
 laser = [Pin(x, Pin.IN) for x in [26,27,28,1,3]] #1 detecta algo
