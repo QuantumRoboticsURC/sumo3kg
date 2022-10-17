d4e6556a91fd9be48a9049ac61068b148052ae22
AriadnaHC <65170456+AriadnaHCTec@users.noreply.github.com>
2022-10-16 23:49:25 -0500

    classes

---
+++ main.py
@@ -0,0 +1,174 @@
+import random
+from machine import Pin, PWM
+import utime
+from sumo import Sumo
+
+class Strategy():
+    def __init__():
+        self.vel_ini = 80
+        self.started = False
+        self.sumo = Sumo()
+
+    def estrategia(self):
+        while True:
+            if not self.sumo.on_button.value() and self.sumo.buttons_value[0] == 0:
+                print("nel")
+                self.sumo.put_velocity(0, 0)
+                self.sumo.read_buttons()
+                utime.sleep(.01)
+            else:        
+                self.sumo.read_values()                        
+                if 0 in self.sumo.ground_back:
+                    self.started = True
+                    self.last_laser = " "
+                    if self.sumo.ground_back[0] == 0 and self.sumo.ground_back[1] == 0:
+                        self.vel = 75
+                        self.sumo.put_velocity(vel,vel)
+                        self.counter_b = 30
+                    elif self.sumo.ground_back[0] == 0:
+                        self.sumo.put_velocity(75,50)
+                        self.counter_b = 20
+                    elif ground_back[1] == 0:
+                        self.sumo.put_velocity(50,75)
+                        self.counter_b = 20
+                elif 0 in self.sumo.ground_front:
+                    self.started = True
+                    self.sumo.last_laser = " "
+                    self.search()
+                    self.sumo.put_state()
+                elif self.state == "back":
+                    self.started = True
+                    if self.counter > 0:
+                        self.counter -= 1                  
+                    else:
+                        if self.last == "both":
+                            self.counter  = 25#Turn
+                        else:                
+                            self.counter  = 20#Turn
+                        self.state = last
+                    self.sumo.put_state()   
+                elif not started:
+                    # Leer segun el boton
+                    if self.sumo.buttons_value[1] == 0:
+                        if self.sumo.buttons_value[2] == 0:
+                            #Frente
+                            if self.sumo.buttons_value[3] == 0:
+                                # Avanzar recto
+                                print("Espalda, girar izquierda")                            
+                                self.put_velocity(-vel_ini,vel_ini)
+                                utime.sleep(.2)
+                            else:
+                                # Avanzar recto (delay)
+                                print("Avanzar recto delay")
+                                utime.sleep(2)
+                                self.sumo.put_velocity(vel_ini,vel_ini)                            
+                        else:
+                            # Espalda                        
+                            if self.sumo.buttons_value[3] == 0:
+                                # Girar Izquierda                            
+                                print("Avanzar recto")
+                                self.sumo.put_velocity(vel_ini,vel_ini)
+                            else:
+                                # Girar Derecha
+                                print("Espalda, girar derecha")
+                                self.sumo.put_velocity(vel_ini,-vel_ini)
+                    else:                    
+                        # Lateral                    
+                        if self.sumo.buttons_value[2] == 0:
+                            # Girar
+                            if self.sumo.buttons_value[3] == 0:
+                                print("Lateral girar izquierda")
+                                # Izquierda
+                                self.sumo.put_velocity(-vel_ini,vel_ini)
+                            else:
+                                # Derecha
+                                print("Lateral girar derecha")
+                                self.sumo.put_velocity(vel_ini,-vel_ini)
+                        else:                        
+                            # Avanzar y luego girar
+                            if self.sumo.buttons[3] == 0:
+                                print("avanzar y luego girar izquierda")
+                                # Izquierda
+                                self.sumo.put_velocity(40,75)
+                                utime.sleep(.2)
+                                self.sumo.put_velocity(-vel_ini,vel_ini)
+                            else:
+                                # Derecha
+                                print("avanzar y luego girar derecha")
+                                self.sumo.put_velocity(vel_ini,vel_ini)
+                                utime.sleep(1)
+                                self.sumo.put_velocity(vel_ini,-vel_ini)   
+                    self.started = True  
+                elif 1 == self.sumo.front:
+                    self.started = True
+                    self.state = "straight"
+                    self.sumo.put_state()
+                    self.last_laser = " "
+                elif 1 in self.sumo.lateral_front:
+                    self.started = True
+                    if self.sumo.lateral_front[0]:
+                        self.sumo.put_velocity(20,75)
+                        self.last_laser = "left"
+                    else:
+                        self.sumo.put_velocity(75,20)
+                        self.last_laser = "right"
+                elif 1 in self.sumo.lateral:
+                    self.started = True
+                    self.state = 'straight'
+                    self.vel = 85
+                    if self.sumo.lateral[0]:           
+                        self.sumo.put_velocity(-vel,vel)
+                        self.last = "left_recio"
+                    else:
+                        self.sumo.put_velocity(vel,-vel)
+                        self.last = "right_recio"                
+                elif self.counter_b == 0:
+                    if self.last_laser == " ":
+                        self.sumo.search()
+                        self.sumo.put_state()
+                    elif self.sumo.last_laser == "right":
+                        self.state = 'straight'
+                        self.sumo.put_velocity(75,20)
+                    elif self.last_laser == "left":
+                        self.state = 'straight'
+                        self.sumo.put_velocity(20,75)
+                    elif self.last == "left_recio":
+                        self.state = 'straight'
+                        self.vel = 85
+                        self.sumo.put_velocity(-vel,vel)
+                    elif last == "right_recio":
+                        self.vel = 85
+                        self.state = 'straight'
+                        self.sumo.put_velocity(vel,-vel)
+                else:
+                    self.counter_b-=1
+                    
+                utime.sleep(.01)
+        
+    def search(self):
+        """Random Search"""
+        if self.state == 'straight':
+            if 0 in self.sumo.ground_front:
+                self.counter = 20
+                self.state = "back"
+                if self.sumo.ground_front[0] == 0 and self.sumo.ground_front[1] == 0:
+                    self.last  = "both"
+                if self.sumo.ground_front[0] == 0:
+                    self.last = "turn_right"
+                else:
+                    self.last = "turn_left"
+        elif self.state == 'turn_left' or self.state == "turn_right":
+            if self.counter > 0:
+                self.counter -= 1     
+            else:
+                self.state = 'straight'
+        elif self.state == "back":
+            if self.counter > 0:
+                self.counter -= 1                  
+            else:
+                if self.last == "both":
+                    self.counter = 25#Turn
+                else:                
+                    self.counter = 20#Turn
+                self.state = last
+        
--- sumo.py
+++ sumo.py
@@ -2,130 +2,98 @@ import random
 from machine import Pin, PWM
 import utime
 
-def main():
-    """General execution and planning"""      
-    start()  
-    while on:
-        read_values()
-        search()
-        put_state()
-        utime.sleep(.1)
 
-def configuration():
-    """Read the buttons to see changes in initial configuration"""
-    if buttons[0].value():
-        start = start_1()
-    else if buttons[1].value():
-        start = start_2()
+class Sumo():
+    def __init__(self):
+        # Declaration of 4 ground sensors (10,12,14,16)
+        self.ground = [Pin(x, Pin.IN) for x in [7,9,10,12]]#1 black 0 blanco
+        self.ground_front = [0]*2
+        self.ground_back = [0]*2
+        self.last = ""
 
-def start_1():
-    print("start 1")
-    pass
+        # Declaration of 4 buttons (24-27)
+        self.buttons = [Pin(x, Pin.IN, Pin.PULL_UP) for x in range(18,22)]
+        self.buttons_value = [0]*len(buttons) #0 = on
+        self.on_button = Pin(4, Pin.IN) # arriba apagado, abajo prendido 
 
-def start_2():
-    print("start 2")
-    pass
+        # Declaration of 5 laser sensors  (31,32,34,2,5)
+        self.laser = [Pin(x, Pin.IN) for x in [26,27,28,1,3]] #1 detecta algo
+        self.lateral = [0]*2
+        self.lateral_front = [0]*2
+        self.front = 0
 
-def search():
-    """First strategy"""
-    global state, counter, ground_front, ground_back 
-    if state == 'straight':
-        if 1 in ground_front or 1 in ground_back:
-            # Change inmediatly
-            state = "stop"
-            counter  = random.randint(5, 14)
-            if ground_front[0]:
-                state = "turn_left"
-            else:
-                state = "turn_right"
-    elif state == 'turn_left' or state == "turn_right":
-        if counter > 0:
-            counter -= 1      
-        else:    
-            state = 'straight'    
+        #PWM
+        self.frequency = 300  #10Khz
+        self.left_wheel = PWM(Pin(17))
+        self.left_wheel.freq(frequency)
+        self.right_wheel = PWM(Pin(16))
+        self.right_wheel.freq(frequency)
+        self.put_velocity(0, 0)
 
-def put_state():
-    """put velocities in one state"""
-    if state == "stop":
-        left_velocity = poss_velocities["low"]
-        right_velocity = poss_velocities["low"]
-    elif state == "straight":
-        left_velocity = poss_velocities["medium"]
-        right_velocity = poss_velocities["medium"]
-    elif state == "turn_left":
-        left_velocity = poss_velocities["high"]
-        right_velocity = poss_velocities["medium"]
-    else:
-        left_velocity = poss_velocities["medium"]
-        right_velocity = poss_velocities["high"]
+        self.state = 'straight'
+        self.counter = 0
+        self.counter_b = 0
+        self.last_laser = " "
 
-def attack():
-    """Search oponent based on laser sensors"""
-    pass
+    def read_values(self):
+        read_ground()
+        read_laser()
 
-def back_off():
-    """Checks 3 input sensors and change velocity"""
-    pass
+    def read_ground(self):
+        self.ground_front[1] = self.ground[2].value()
+        self.ground_front[0] = self.ground[3].value()
+        self.ground_back[0] = self.ground[1].value()
+        self.ground_back[1] = self.ground[0].value()
+        #print("ground_front: "+str(ground_front)+"      ground_back: "+str(ground_back))
 
-def read_values():
-    read_ground()
+    def read_laser(self):
+        #[31,32,34,2,5]
+        self.lateral[0] = self.laser[4].value()
+        self.lateral[1] = self.laser[3].value()
+        self.lateral_front[0] = self.laser[1].value()
+        self.lateral_front[1] = self.laser[2].value()
+        self.front = self.laser[0].value()
+        #print("lateral: "+str(lateral)+"      lateral_front: "+str(lateral_front)+"       front: "+str(front))
 
-def read_ground():
-    global ground_front, ground_back
-    for i in range(2):
-        ground_front = ground[i].value()
-        ground_back = ground[i+2].value()
+    def read_buttons(self):
+        for i in range(len(buttons)):
+            self.buttons_value[i] = self.buttons[i].value()
+        #print("buttons: "+str(buttons_value))
+    
+    def _map(self,x, in_min, in_max, out_min, out_max):
+        return int((x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min)
 
-def read_laser():    
-    global laser_value, lateral, lateral_front, front
-    #[31,32,34,2,5]
-    lateral[0] = laser[3].value()
-    lateral[1] = laser[4].value()
-    lateral_front[0] = laser[2].value()
-    lateral_front[1] = laser[1].value()
-    front = laser[0].value()    
+    def put_velocity(self, left, right):    
+        self.left_wheel.duty_ns(_map(left,-100,100,2000000,1000000))
+        self.right_wheel.duty_ns(_map(right,-100,100,2000000,1000000))
 
-def read_buttons():
-    global buttons_value
-    for i in range(len(buttons)):
-        buttons_value[i] = buttons[i].value()
+    def put_state(self):
+        """put velocities in one state"""
+        if state == "stop":
+            self.put_velocity(0,0)
+        elif state == "straight":
+            vel = 70
+            self.put_velocity(vel,vel)
+        elif state == "turn_left":
+            vel = 75
+            self.put_velocity(-vel,vel)
+        elif state == "turn_right":
+            vel = 75
+            self.put_velocity(vel,-vel)
+        elif state == "back":
+            vel = -70
+            self.put_velocity(vel,vel)
 
-poss_velocities = {"low":0,"medium":15,"high":30}
-left_velocity = poss_velocities["medium"]
-right_velocity = poss_velocities["medium"]
+    def test_sensors(self):
+        while True:        
+            self.read_values()
+            utime.sleep(.5)
 
-# Declaration of 4 ground sensors (10,12,14,16)
-ground = [Pin(x, Pin.IN) for x in [7,9,10,12]]
-ground_front = [0]*2
-ground_back = [0]*2
-
-# Activation
-# Declaration of 2 pwm (21,22)
-frequency = 1000  #10Khz
-left_wheel = PWM(Pin(16))
-left_wheel.freq(frequency)
-right_wheel = PWM(Pin(17))
-right_wheel.freq(frequency)
-#led.duty_u16(1)
-
-# Declaration of 5 laser sensors  (31,32,34,2,5)
-laser = [Pin(x, Pin.IN) for x in [26,27,28,1,3]]
-lateral = [0]*2, lateral_front = [0]*2, front = 0
-
-# Declaration of 4 buttons (24-27)
-buttons = [Pin(x, Pin.IN, Pin.PULL_UP) for x in range(18,22)]
-buttons_value = [0]*len(buttons)
-on_button = Pin(12, Pin.IN, Pin)
-
-while on_button.value()!=0:
-    utime.sleep(.01)
-
-on = True
-state = 'straight'
-counter = 0
-start = None
-
-
-#configuration()
-main()
-
+    def test_pwm(self):
+        while True:        
+            for i in range(50,90,10):
+                print(i)
+                per_ = self._map(i,-100,100,2000000,1000000)
+                self.left_wheel.dutzy_ns(per_)
+                self.right_wheel.dutzy_ns(per_)
+                utime.sleep(10)
